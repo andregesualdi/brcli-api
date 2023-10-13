b1ae9985eaece2c782d3fdf1faa5d446387bb2f3
andregesualdi <andrerges@gmail.com>
2023-10-12 21:47:52 -0300

    Ajustes para a rota de Agenda do Desktop

--- src/models/agenda.model.js
+++ src/models/agenda.model.js
@@ -2,15 +2,17 @@ class Agenda {
     constructor(
         nome,
         imagem,
-        hora
+        hora,
+        codigoPaciente
     ) {
         this.nome = nome;
         this.imagem = imagem;
         this.hora = hora;
+        this.codigoPaciente = codigoPaciente;
     }
 
     static selectAgenda = function(id, data) {
-        return `SELECT p.nome, p.imagem, a.horaAgendamento FROM pacientes p
+        return `SELECT p.idPaciente, p.nome, p.imagem, a.horaAgendamento FROM pacientes p
         JOIN agendamentos a ON p.usuarios_id = a.usuarios_id AND p.idPaciente = a.pacientes_idPaciente
         WHERE p.usuarios_id = ${id} AND a.dataAgendamento = '${data}';`
     }
--- src/usecases/agenda/getAgenda.js
+++ src/usecases/agenda/getAgenda.js
@@ -5,7 +5,7 @@ export default function makeGetAgenda(db) {
         const response = await db.selectAgenda(headers['codigo-usuario'], headers['data']);
         if (response[0] && response[0].length > 0) {
             let agenda = response[0].map((el) => {
-                return new Agenda(el.nome, el.imagem, el.horaAgendamento);
+                return new Agenda(el.nome, el.imagem, el.horaAgendamento, el.idPaciente);
             });
             return agenda;
         } else {
